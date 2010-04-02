ac841650c77a72f2c4ee3f05cd3b13d9aa68a649
hukl <contact@smyck.org>
2010-04-03 01:07:16 +0200

    added admin views for basic post management

--- lib/admin.rb
+++ lib/admin.rb
@@ -7,9 +7,38 @@ module Admin
     end
     
     get '/?' do
-      "hallo from the admin"
+      @posts = Post.all( :order => '_id DESC')
+      erb :'admin/index', :layout => :'admin/layout'
     end
     
+    get '/posts/new/?' do
+      @post = Post.new params[:post]
+      erb :'admin/new', :layout => :'admin/new'
+    end
+    
+    post '/posts/?' do
+      @post = Post.new params[:post]
+      if @post.save
+        redirect '/admin'
+      else
+        erb :'admin/new', :layout => :'admin/new'
+      end
+    end
+    
+    get '/posts/:id/edit' do
+      @post = Post.find(params[:id])
+      erb :'admin/edit', :layout => :'admin/layout'
+    end
+    
+    post '/posts/:id/?' do
+      @post = Post.find(params[:id])
+      
+      if @post.update_attributes( params[:post] )
+        redirect '/admin/'
+      else
+        erb :'admin/edit', :layout => :'admin/layout'
+      end
+    end
   end
   
 end
