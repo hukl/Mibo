98ba252c58c8b1671b8ace4bda03973eaba00b64
hukl <contact@smyck.org>
2010-04-06 23:29:43 +0200

    added tags to posts

--- lib/post.rb
+++ lib/post.rb
@@ -3,4 +3,18 @@ class Post
   
   key :title, :required => true
   key :body
+  key :tag_list, Array
+  
+  
+  def tags
+    tag_list
+  end
+  
+  def tags= list_of_tags
+    if list_of_tags.is_a?( String )
+      @tag_list = list_of_tags.split(",")
+    elsif list_of_tags.is_a?( Array )
+      @tag_list = list_of_tags
+    end
+  end
 end
